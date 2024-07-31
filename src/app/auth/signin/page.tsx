a50052e8cc5d5395dbbe8fbd850944cc7ea9b6d0
Andrelino Silva <andrelinodev@gmail.com>
2024-07-31 13:20:53 -0300

    update

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -20,32 +20,31 @@ export default async function SignIn() {
   const providers = await getProviders()
 
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4 ">
-      {providers &&
-        Object.values(providers).map((provider) => (
-          <div key={provider.name} className="flex w-full max-w-xs p-4">
-            {provider.id !== 'email' ? (
-              <ButtonProvider key={provider.id} provider={provider} />
-            ) : (
-              <form
-                method="post"
-                action="/api/auth/signin/email"
-                className="flex w-full flex-col gap-4 "
-              >
-                <Input
-                  name="csrfToken"
-                  type="hidden"
-                  defaultValue={csrfToken}
-                />
-                <Label className="flex flex-col">
-                  Email address
-                  <Input type="email" id="email" name="email" />
-                </Label>
-                <Button type="submit">Acessar com e-mail</Button>
-              </form>
-            )}
-          </div>
-        ))}
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-4">
+      <div className="flex w-full max-w-xs flex-col items-center justify-center gap-4 rounded-lg border p-4 ">
+        <h2 className="font-semibold">Login via rede social</h2>
+        {providers &&
+          Object.values(providers).map((provider) => (
+            <div key={provider.name} className="flex w-full max-w-xs">
+              {provider.id !== 'email' && (
+                <ButtonProvider key={provider.id} provider={provider} />
+              )}
+            </div>
+          ))}
+      </div>
+      <form
+        method="post"
+        action="/api/auth/signin/email"
+        className="flex w-full max-w-xs flex-col gap-4 rounded-lg border p-4 "
+      >
+        <h2 className="font-semibold">Login via email</h2>
+        <Input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+        <Label className="flex flex-col gap-2">
+          Endereço de e-mail
+          <Input type="email" id="email" name="email" />
+        </Label>
+        <Button type="submit">Acessar com e-mail</Button>
+      </form>
     </div>
   )
 }
